2fbd38beda5e9fa154bf9130485856948d33871d
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-09-01 17:40:42 +0530

    Added the LINQ Extension

--- ExtensionMethod/Program.cs
+++ ExtensionMethod/Program.cs
@@ -11,24 +11,28 @@ class Program {
         WriteLine (email.IsValidEmail ());
         WriteLine (str);
         WriteLine ();
+
         //Integer Extension
         WriteLine (4.IsEven ());
         WriteLine (144.ReverseSquare ());
         WriteLine ();
+
         //Inheritance with extension
         var myDog = new Dog ();
         myDog.Speak ();
         myDog.Identify ();
         WriteLine ();
+
         // Collection Extension
         List<int> numbers = [10, 20, 30, 40, 50];
         string[] arr = ["Trumpf", "Metamation"];
         WriteLine ($"Is Empty: {numbers.IsEmpty ()}");
-        WriteLine ("Printing numbers in Lsit.");
+        WriteLine ($"Printing numbers in List.");
         numbers.PrintItems ();
         WriteLine ("Printing string in array.");
         arr.PrintItems ();
         WriteLine ();
+
         // Class Extension
         Employee emp = new (1, "John", "IT", 75000.0m, 6);
         WriteLine (emp.IsExperienced ());
@@ -38,6 +42,19 @@ class Program {
                      new(2, "David", "HR", 65000.0m, 3),
                      new(3, "Sarah", "Finance", 85000.0m, 8) ];
         WriteLine (empList.GetSummary ());
+        WriteLine ();
+
+        // LINQ Extension 
+        List<Employee> employees =
+        [
+            new Employee{ Id = 1,Name = "John",Department = "Development",Salary = 75000,Experience = 7},
+            new Employee{Id = 2,Name = "David",Department = "Testing",Salary = 45000,Experience = 3},
+            new Employee{Id = 3,Name = "Sam",Department = "Development",Salary = 65000,Experience = 6},
+            new Employee{Id = 4,Name = "Alex",Department = "HR",Salary = 40000,Experience = 2}
+        ];
+        var highestSalary = employees.Where (emp => emp.Salary > 50000).ToList();
+        WriteLine (employees.GetSummary());
+        WriteLine ();
     }
     #endregion
 }
@@ -62,6 +79,7 @@ public static class StingExtension {
 #region Integer Extension -------------------------------------------------------------------------
 public static class IntegerExtension {
     public static bool IsEven (this int value) => value % 2 == 0;
+
     public static int ReverseSquare (this int value) {
         if (value < 0) ArgumentException.ThrowIfNullOrEmpty ("Number cannot be negative(-).");
         if (value < 2) return value;
@@ -93,11 +111,12 @@ public class Dog : Animal {
 
 public static class AnimalExtension {
     public static void Identify (this Animal animal) => WriteLine ("I am an Animal extension");
+
     public static void Identify (this Dog dog) => WriteLine ("I am a Dog extension");
 }
 #endregion
 
-#region Class Extension --------------------------------------------------------------------------
+#region class Extension ---------------------------------------------------------------------------
 public class Employee {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
@@ -105,6 +124,7 @@ public class Employee {
     public decimal Salary { get; set; }
     public int Experience { get; set; }
 
+    #region Parameterized Constructor -------------------------------
     public Employee (int id, string name, string dept, decimal salary, int exp) {
         Id = id;
         Name = name;
@@ -112,11 +132,18 @@ public class Employee {
         Salary = salary;
         Experience = exp;
     }
+    #endregion
+
+    #region Non-Parameterized Constructor ---------------------------
+    public Employee () {}
+    #endregion
 }
 
 public static class EmployeeExtension {
     public static bool IsExperienced (this Employee emp) => emp.Experience >= 5;
+
     public static bool IsHighSalary (this Employee emp) => emp.Salary > 50000;
+
     public static string GetSummary (this List<Employee> employees) =>
     string.Join (Environment.NewLine, employees.Select (emp =>
             $"{emp.Name} | {emp.Department} | " +
